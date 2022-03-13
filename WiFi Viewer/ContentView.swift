badb39022f4368e79e01b5757b533dba06710e1d
Nikolay Botev <nikolaybotev.b@gmail.com>
2022-03-13 17:04:27 +0200

    First commit

---
+++ WiFi Viewer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WiFi Viewer
+//
+//  Created by Nikolay Botev on 3/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WiFi Viewer/WiFi_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiFi_ViewerApp.swift
+//  WiFi Viewer
+//
+//  Created by Nikolay Botev on 3/13/22.
+//
+
+import SwiftUI
+
+@main
+struct WiFi_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
